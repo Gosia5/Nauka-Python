751aa56abd6734b2edc739b990dbefe2d06e7418
Malgorzata <malgorzata.swietlik1@gmail.com>
2026-07-30 12:37:13 +0200

    Wpisz hasło - 3 próby - For

--- Nauka_Python.py
+++ Nauka_Python.py
@@ -1,9 +1,20 @@
 
-for i in range (5):
-    print(i)
+#for i in range (5):
+#    print(i)
 
-i = 0 
-while i < 5:
-    print(i)
-    i += 1
+#i = 0 
+#while i < 5:
+#    print(i)
+#    i += 1
 
+haslo="Haslo123"
+licznik=0
+for i in range(3):
+    proba=input("Podaj hasło: ")
+    if proba == haslo:
+        print("Hasło poprawne.")
+        break
+    else:
+        print("Błąd.")
+else:
+    print("Za dużo prób.")
